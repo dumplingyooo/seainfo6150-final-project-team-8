c88c3faf153a99dbbb38155921399859b954bab4
dumplingyooo <lishujin1125@gmail.com>
2019-12-01 12:50:18 -0800

    modify 404 page

--- src/RecipeWeb/Error.jsx
+++ src/RecipeWeb/Error.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import error from "./404.jpg"
+import styles from "./css/Error.module.css";
+import {Link} from "react-router-dom";
 
 const Error = () => {
     return (
         <div>
-            The error page
+            <p>Oops!</p>
+            <p>We can't seem to find the page you're looking for.</p>
+            <p>Error code: 404</p>
+            <p>Here are some helpful links instead:</p>
+            <p>
+            <Link className={styles.link_blue} to="/">
+					Return to home
+			</Link>
+            </p>
+            <p>
+            <Link className={styles.link_blue} to="/AllCategories/appetizer">
+					Return to appetizer
+			</Link>
+            </p>
+            <Link className={styles.link_blue} to="/AllCategories/mainCourse">
+					Return to mainCourse
+			</Link>
+            <p>
+            <Link className={styles.link_blue} to="/AllCategories/dessert">
+					Return to dessert
+			</Link>
+            </p>
+            <img src={error} alt="404" title="404"></img>
         </div>
     )
 }
 
-export default Error
+export default Error;
